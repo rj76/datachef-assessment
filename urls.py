c0e688e45c1da71f406ed8a1155399a2816909d9
Richard Jansen <richard@pedroja.tech>
2021-06-03 20:57:59 +0200

    added debug_toolbar and campaign endpoint

--- urls.py
+++ urls.py
@@ -1,10 +1,13 @@
 """datachef_assessment URL Configuration
 """
-from django.urls import path
+import debug_toolbar
+from django.urls import path, include
 
-from apps.banner.views import HomeView
+from apps.banner import views
 
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('campaign/<int:pk>/', views.CampaignDetail.as_view(), name='campaign'),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
