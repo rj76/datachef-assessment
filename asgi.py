3367c849f5ec3d64282f1cb78aaf44334bc43129
Richard Jansen <richard@pedroja.tech>
2021-06-03 11:19:25 +0200

    wrong module name; added live config

--- asgi.py
+++ asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datachef_assessment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'live_settings')
 
 application = get_asgi_application()
